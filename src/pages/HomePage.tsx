3239ffde044d709225d4517c86a97513e172ecab
Abhinav <abhinav.contact22@gmail.com>
2024-04-18 21:10:23 +0530

    Changes in Pages

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,34 +1,13 @@
-import {
-  allCourseAtom,
-  selectedCourse,
-  selectedCourseId,
-} from "@/store/atoms/atom";
 import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { Course } from "@/constants/interfaces";
 import Link from "next/link";
+import axios from "axios";
 
-function HomePage() {
-  const courseArr = useRecoilValue(allCourseAtom);
-
-  useEffect(() => {
-    alert(typeof Array.from(courseArr));
-  });
-  return (
-    <div>
-      {courseArr.map((course: Course) => {
-        return (
-          <Link href={`courses/${course.id}`}>
-            <div className="m-4 p-3 shadow-md rounded-xl">
-              <h1 className="text-2xl font-bold">{course.title}</h1>
-              <h2 className="font-semibold pt-1.5">{course.description}</h2>
-              <p>{course.publisherId}</p>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
-  );
+async function HomePage() {
+  const resp = await axios.post(`v0/api/course`);
+  const coursesArray = resp.data;
+  return <div></div>;
 }
 
 export default HomePage;
