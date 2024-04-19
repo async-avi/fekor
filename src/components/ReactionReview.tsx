325afc47c77167ab134e9dce3a4faa8ff8cc4fe5
Abhinav <abhinav.contact22@gmail.com>
2024-04-19 15:21:46 +0530

    Added reaction review section

---
+++ src/components/ReactionReview.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ReactionReview() {
+  return (
+    <div className="w-44 h-44 bg-gray-800 rounded-lg flex flex-col px-3">
+      <div className="h-1/2 w-full justify-center items-center flex">
+        <h1 className="text-4xl">&#128175;</h1>
+      </div>
+      <div className="text-white">
+        <h1 className="text-base font-semibold">Reviews</h1>
+        <h2 className="font-bold text-xl">200k+</h2>
+      </div>
+    </div>
+  );
+}
+
+export default ReactionReview;
