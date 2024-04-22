ffcf3790ba8194ee06e8f857304b85bbaa7881fa
Abhinav <abhinav.contact22@gmail.com>
2024-04-22 22:14:03 +0530

    Single Course UI updation

--- src/components/SingleCourse.tsx
+++ src/components/SingleCourse.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Voting from "./Voting";
 import WatchHours from "./WatchHours";
 import ReactionReview from "./ReactionReview";
+import Reviews from "./Reviews";
 
 function SingleCourse({ course }: { course: Course }) {
   const {
@@ -20,6 +21,7 @@ function SingleCourse({ course }: { course: Course }) {
     clicksReceived,
     isSponsored,
     publisherId,
+    reviews,
   } = course;
 
   return (
@@ -35,9 +37,26 @@ function SingleCourse({ course }: { course: Course }) {
           watchHours={watchHours}
           actualWatchHours={actualWatchHours}
         />
-        <ReactionReview />
+        <ReactionReview totalReviews={reviews.length} />
       </div>
       <Voting upVotes={upVotes} downVotes={downVotes} />
+      {reviews.length > 0 ? (
+        <div>
+          <h1 className="text-2xl font-semibold">Reviews: {reviews.length}</h1>
+          {reviews.map((review) => {
+            return <Reviews {...review} />;
+          })}
+        </div>
+      ) : (
+        <div className="flex flex-col h-56">
+          <h1>Reviews: 0</h1>
+          <div className="h-full flex justify-center items-center shadow-2xl">
+            <h1 className="opacity-45 text-gray-900">
+              Be the first one to add a review
+            </h1>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
