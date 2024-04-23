8ed9163ddfa9ab74314d66d0ea1d64a5144ab6cd
Abhinav <abhinav.contact22@gmail.com>
2024-04-23 20:49:32 +0530

    Sponsered posts priority

--- src/app/v0/api/course/route.ts
+++ src/app/v0/api/course/route.ts
@@ -2,5 +2,15 @@ import { coursesArr } from "@/constants/courses.demo";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(coursesArr);
+  let customArr = [];
+
+  const sponserdCourses = coursesArr.filter(
+    (course) => course.isSponsored === true
+  );
+
+  for (let i = 0; i < 3; i++) {
+    customArr.push(sponserdCourses[i]);
+  }
+
+  return NextResponse.json(customArr);
 }
