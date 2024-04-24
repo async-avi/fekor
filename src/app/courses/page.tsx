dc096cc8254fb0c5239cb38aecb38af5dd59182e
Abhinav <abhinav.contact22@gmail.com>
2024-04-24 20:11:26 +0530

    Brushed up courses page

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,37 +1,35 @@
 import axios from "axios";
 import { Course } from "@/constants/interfaces";
-import Link from "next/link";
-import CategoryTab from "@/components/CategoryTab";
+import CourseCard from "@/components/CourseCard";
 
 async function fetchData() {
-  const res = await axios.get(`http://localhost:3000/v0/api/course`);
-  return res.data;
+  const res = await fetch(`http://localhost:3000/v0/api/course`, {
+    cache: "force-cache",
+  });
+  const result = await res.json();
+  return result;
 }
 export default async function page() {
   const data: Course[] = await fetchData();
+  console.log(data.length);
 
   return (
     <>
-      {data.map((course: Course) => {
-        const isHot = course.category.find((c) => c == "hot");
-        return (
-          <Link href={`courses/${course.id}`} key={course.id}>
-            <div className="m-4 p-3 shadow-md rounded-xl">
-              <h1 className="text-2xl font-bold">{course.title}</h1>
-              <div className="flex gap-2">
-                {course.isSponsored ? (
-                  <CategoryTab category="Sponsored" />
-                ) : null}
-                {isHot ? (
-                  <CategoryTab category="Hot" bgColor="bg-gray-800" />
-                ) : null}
-              </div>
-              <h2 className="font-semibold pt-1.5">{course.description}</h2>
-              <p>{course.publisherId}</p>
-            </div>
-          </Link>
-        );
-      })}
+      <div className="px-2">
+        {data.map((course) => {
+          return (
+            <CourseCard
+              key={course.id}
+              category={course.category}
+              description={course.description}
+              id={course.id}
+              isSponsered={course.isSponsored}
+              publisherId={course.publisherId}
+              title={course.title}
+            />
+          );
+        })}
+      </div>
     </>
   );
 }
