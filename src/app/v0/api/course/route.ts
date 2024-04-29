5a8ca2550e82df7e7b97f557901feb2ab37eafda
Abhinav <abhinav.contact22@gmail.com>
2024-04-30 00:31:17 +0530

    Changing api for test cases

--- src/app/v0/api/course/route.ts
+++ src/app/v0/api/course/route.ts
@@ -8,8 +8,16 @@ export async function GET() {
     (course) => course.isSponsored === true
   );
 
+  const normalCourses = coursesArr.filter(
+    (course) => course.isSponsored !== true
+  );
+
   for (let i = 0; i < 3; i++) {
     customArr.push(sponserdCourses[i]);
+  }
+
+  for (let i = 0; i < normalCourses.length; i++) {
+    customArr.push(normalCourses[i]);
   }
 
   return NextResponse.json(customArr);
